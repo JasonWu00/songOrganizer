68a1778b3bac2d1d66a33c8c6323f3cb2180e10d
Jason Wu <zwu00@stuy.edu>
2019-10-31 11:59:11 -0400

    removeNode is broken, trying to fix rn

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main() {
   printList(headOfList);
   printf("\n");
 
+  //headOfList = calloc(sizeof(struct node), 1);
   printf("populating the list\n\n");
 
   headOfList = insertAtFront(headOfList, "Tears in Heaven", "Eric Clapton");
@@ -23,8 +24,8 @@ int main() {
   printList(headOfList);
   printf("\n\n");
 
-  printf("Finding hey jude:\n");
-  printSingleNode(findSong(headOfList, "Hey Jude"));
+  printf("Finding crossroads:\n");
+  printSingleNode(findSong(headOfList, "Crossroads"));
   printf("\n");
   printf("Finding imagine:\n");
   printSingleNode(findSong(headOfList, "Imagine"));
@@ -39,6 +40,10 @@ int main() {
   printf("removing middle node, value of node should be \"Eric Clapton | Crossroads\"\n");
   headOfList = removeNode(headOfList, "Crossroads", "Eric Clapton");
   printf("printing list\n");
+  printf("\n");
+  printf("removing node of \"StringStorm | Cadian Blood\", which does not exist\n");
+  headOfList = removeNode(headOfList, "Cadian Blood", "StringStorm");
+
   printList(headOfList);
   printf("\n\n");
 
--- nodes.c
+++ nodes.c
@@ -4,12 +4,19 @@
 #include "nodes.h"
 
 void printList(struct node *headOfList) {
-  if(headOfList == NULL) {printf("List is empty");}
+  if(headOfList == NULL) {
+    printf("[ List is empty ]");
+  }
   else {
     printf("[");
     while(headOfList != NULL) {
-      printf("%s | ", headOfList->artist);
-      printf("%s ; ", headOfList->songName);
+      if (headOfList->songName == NULL && headOfList->artist == NULL) {
+        printf("");
+      }
+      else {
+        printf("%s | ", headOfList->artist);
+        printf("%s ; ", headOfList->songName);
+      }
       headOfList = headOfList->nextNode;
     }
     printf("]");
@@ -24,25 +31,44 @@ struct node * insertAtFront(struct node *headOfList, char *newSong, char *newArt
   return newHead;
 }
 
-struct node * removeNode(struct node *headOfList, char *targetSong, char *targetArtist) {
+struct node * removeNode(struct node *headOfList, const char *targetSong, const char *targetArtist) {
   //renamed method due to conflict with existing inbuilt remove
+  char *song = targetSong;
+  char *artist = targetArtist;
   struct node *next = headOfList->nextNode;
-  int isEqual = strcmp(next->songName, targetSong) + strcmp(next->artist, targetArtist);
-  if (headOfList->nextNode != NULL && isEqual != 0) {
-    removeNode(headOfList->nextNode, targetSong, targetArtist);
+  int nextIsEqual = strcmp(next->songName, song) + strcmp(next->artist, artist);
+  int thisIsEqual = strcmp(headOfList->songName, song) + strcmp(headOfList->artist, artist);
+
+  if (thisIsEqual == 0) {
+    printf("Removing node with the value of %s | %s\n", headOfList->artist, headOfList->songName);
+    struct node *next = headOfList->nextNode;
+    free(headOfList);
+    return next;
   }
-  if (headOfList->nextNode == NULL) {
-    printf("Node with value of %s | %s not found\n", *targetArtist, *targetSong);
-    return headOfList;
-  }
-  if (isEqual == 0) {
+
+  if (nextIsEqual == 0) {
     struct node *restOfList = headOfList->nextNode->nextNode;
     struct node *ax = headOfList->nextNode;
     printf("Removing node with the value of %s | %s\n", ax->artist, ax->songName);
     free(ax);
     headOfList->nextNode = restOfList;
+    return headOfList;
   }
+
+  if (headOfList->nextNode != NULL && nextIsEqual != 0) {
+    removeNode(headOfList->nextNode, targetSong, targetArtist);
+  }
+
+  if (headOfList->nextNode == NULL) {
+    printf("Node with value of %s | %s not found\n", targetArtist, targetSong);
+    return headOfList;
+  }
+  /*
+  struct node nodeToAx = findSongByArtist(headOfList, targetSong);
+  printf("Removing node with the value of %s | %s\n", nodeToAx->artist, nodeToAx->songName);
+  free(nodeToAx);
   return headOfList;
+  */
 }
 
 struct node * freeList(struct node *headOfList) {
